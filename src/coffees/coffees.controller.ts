f6c9ace0bcd47b5844d0b75b2e1607925e1acce6
ahsn-dev <ahsn.dev@gmail.com>
2024-03-05 10:15:52 +0330

    add controller for coffees

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll(@Query() paginationQuery): string {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body): string {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string): string {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `This action removes #${id} coffee`;
+  }
+}
