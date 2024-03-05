3fd41ce75228e5aceb3e311b966ef3fa074cf50c
ahsn-dev <ahsn.dev@gmail.com>
2024-03-05 11:14:05 +0330

    add service and entity for coffee

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,35 +8,37 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
-  findAll(@Query() paginationQuery): string {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+  findAll() {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns #${id} coffee`;
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.GONE)
-  create(@Body() body): string {
-    return body;
+  create(@Body() body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string): string {
-    return `This action updates #${id} coffee`;
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return `This action removes #${id} coffee`;
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Amir',
+      brand: 'Buddy Brew',
+      flavors: ['vanilla', 'chocolate'],
+    },
+    {
+      id: 2,
+      name: 'Ali',
+      brand: 'Starbucks',
+      flavors: ['vanilla', 'chocolate', 'caramel'],
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: string): Coffee {
+    const coffee = this.coffees.find((coffee) => coffee.id === parseInt(id));
+    if (!coffee) {
+      //   throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException(`Coffee #${id} not found`); // This is a new feature in NestJS 8 instead of the code above.
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      existingCoffee.name = updateCoffeeDto.name;
+      existingCoffee.brand = updateCoffeeDto.brand;
+      existingCoffee.flavors = updateCoffeeDto.flavors;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex(
+      (coffee) => coffee.id === parseInt(id),
+    );
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
