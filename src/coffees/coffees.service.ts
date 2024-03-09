a1c5a53138faf3d025588344336596ace4a500a7
ahsn-dev <ahsn.dev@gmail.com>
2024-03-09 15:01:53 +0330

    add flavor & coffee

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,11 +13,15 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
-  async findOne(id) {
-    const coffee = await this.coffeeRepository.findOne(id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne(id, {
+      relations: ['flavors'],
+    });
     if (!coffee) {
       //   throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
       throw new NotFoundException(`Coffee #${id} not found`); // This is a new feature in NestJS 8 instead of the code above.
